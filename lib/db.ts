361bfa29fe9fc52b5108cbb43ec0ad0f9119c1da
Ishaan Dey <ishaan1013@gmail.com>
2025-05-11 16:48:05 -0400

    db

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+import { Pool } from "pg";
+
+if (
+  !process.env.POSTGRES_URL ||
+  !process.env.POSTGRES_USER ||
+  !process.env.POSTGRES_HOST ||
+  !process.env.POSTGRES_DATABASE ||
+  !process.env.POSTGRES_PASSWORD
+) {
+  throw new Error("Missing environment variables");
+}
+
+const db = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
+
+export default db;
