1623046185afc1c9679b55d054319c783b1cb0b2
Elijah Kurien <elijahkurien05@gmail.com>
2025-05-11 17:09:20 -0400

    added queries

---
+++ database/queries/temp_queries.sql
@@ -0,0 +1,7 @@
+-- Use these queries in the code for milestone 0
+
+FROM users SELECT * FROM users;
+
+FROM users SELECT * FROM users WHERE favorite_color = 'red';
+
+FROM users SELECT * FROM users WHERE favorite_color = 'red' ORDER BY name;
