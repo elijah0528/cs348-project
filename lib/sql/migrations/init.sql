556794d143870575346ffeda17c92faee5a4d64c
Elijah Kurien <elijahkurien05@gmail.com>
2025-05-26 15:40:08 -0400

    initial schema

--- lib/sql/migrations/init.sql
+++ lib/sql/migrations/init.sql
@@ -1,5 +1,38 @@
-CREATE TABLE test (
-    id UUID PRIMARY KEY,
+CREATE TABLE profiles (
+    user_id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    pw_hash TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE subreddits (
+    subreddit_id UUID PRIMARY KEY,
+    subreddit_name TEXT NOT NULL UNIQUE,
+    user_id UUID REFERENCES profiles(user_id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE posts (
+    post_id UUID PRIMARY KEY,
+    user_id UUID REFERENCES profiles(user_id),
+    title TEXT NOT NULL,
     content TEXT NOT NULL,
-    country TEXT NOT NULL
+    subreddit_id UUID REFERENCES subreddits(subreddit_id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    comment_id UUID PRIMARY KEY,
+    content TEXT NOT NULL,
+    post_id UUID REFERENCES posts(post_id),
+    subreddit_id UUID REFERENCES subreddits(subreddit_id),
+    user_id UUID REFERENCES profiles(user_id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE subreddit_membership (
+    user_id UUID REFERENCES profiles(user_id),
+    subreddit_id UUID REFERENCES subreddits(subreddit_id),
+    PRIMARY KEY (user_id, subreddit_id)
 );
