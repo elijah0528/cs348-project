e7f953789b866997139da6536408a7a3e5b00fa3
Rajan Agarwal <rajan.ag005@gmail.com>
2025-06-19 00:31:15 -0400

    update readme to latest

--- m0/feature1.sql
+++
@@ -1,9 +0,0 @@
--- for testing purposes (before)
-SELECT * FROM profiles;
-
--- logic
-INSERT INTO profiles (user_id, username, email, pw_hash)
-VALUES ('00000000-0000-0000-0000-000000000007', 'Rajan Agarwal', 'rajan.agarwal@uwaterloo.ca', 'newpass');
-
--- also for testing purposes (after)
-SELECT * FROM profiles;
--- m0/feature2.sql
+++
@@ -1,9 +0,0 @@
--- for testing purposes (before)
-SELECT * FROM profiles;
-
--- logic
-DELETE FROM profiles
-WHERE user_id = '00000000-0000-0000-0000-000000000007';
-
--- also for testing purposes (after)
-SELECT * FROM profiles;
