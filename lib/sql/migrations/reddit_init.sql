b92bafa6a5bb5383097648648fb8f81ac6ae75b6
Rajan Agarwal <64426829+rajansagarwal@users.noreply.github.com>
2025-06-19 21:36:23 -0400

    Update reddit_init.sql

--- lib/sql/migrations/reddit_init.sql
+++ lib/sql/migrations/reddit_init.sql
@@ -15,7 +15,7 @@ CREATE TABLE subreddits (
 
 CREATE TABLE posts (
     post_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
-    user_id UUID REFERENCES profiles(user_id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES profiles(user_id) ON DELETE CASCADE,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
     subreddit_id UUID NOT NULL REFERENCES subreddits(subreddit_id) ON DELETE CASCADE,
@@ -25,8 +25,8 @@ CREATE TABLE posts (
 CREATE TABLE comments (
     comment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     content TEXT NOT NULL,
-    post_id UUID REFERENCES posts(post_id) ON DELETE CASCADE,
-    user_id UUID REFERENCES profiles(user_id) ON DELETE CASCADE,
+    post_id UUID NOT NULL REFERENCES posts(post_id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES profiles(user_id) ON DELETE CASCADE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
