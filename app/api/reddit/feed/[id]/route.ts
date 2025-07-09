c2159e13288248671581ca46413aaa3d31bfdbc7
Rajan Agarwal <rajan.ag005@gmail.com>
2025-07-08 21:55:42 -0400

    bring back original correct recentQuery (what happened to it??)

--- app/api/reddit/feed/[id]/route.ts
+++ app/api/reddit/feed/[id]/route.ts
@@ -7,35 +7,17 @@ export async function GET(request: NextRequest, context: { params: Promise<{ id:
     const { searchParams } = new URL(request.url);
     const sort = searchParams.get("sort") === "popular" ? "popular" : "recent";
 
-    const recentQuery = `WITH all_posts AS (
-       SELECT p.post_id, p.user_id, p.title, p.content, p.created_at, s.subreddit_name, pr.username, SUM(v.vote_type) AS vote_score
-       FROM posts p
-       JOIN subreddits s ON s.subreddit_id = p.subreddit_id
-       JOIN profiles pr ON pr.user_id = p.user_id
-       JOIN votes v ON v.post_id = p.post_id
-       WHERE (
-         p.subreddit_id IN (SELECT subreddit_id FROM subreddit_membership WHERE user_id = $1)
-         OR p.subreddit_id IN (SELECT subreddit_id FROM subreddits WHERE admin_id = $1)
-       )
-       GROUP BY p.post_id, p.user_id, p.title, p.content, p.created_at, s.subreddit_name, pr.username
-
-       UNION ALL
-
-       SELECT p.post_id, p.user_id, p.title, p.content, p.created_at, s.subreddit_name, pr.username, 0 AS vote_score
-       FROM posts p
-       JOIN subreddits s ON s.subreddit_id = p.subreddit_id
-       JOIN profiles pr ON pr.user_id = p.user_id
-       WHERE (
-         p.subreddit_id IN (SELECT subreddit_id FROM subreddit_membership WHERE user_id = $1)
-         OR p.subreddit_id IN (SELECT subreddit_id FROM subreddits WHERE admin_id = $1)
-       )
-         AND NOT EXISTS (
-           SELECT 1 FROM votes v WHERE v.post_id = p.post_id
-         )
-     )
-     SELECT *
-     FROM all_posts
-     ORDER BY created_at DESC`;
+    const recentQuery = `SELECT p.post_id, p.user_id, p.title, p.content, p.created_at, pr.username, s.subreddit_name, s.subreddit_id, SUM(v.vote_type) AS vote_score
+        FROM posts p
+        LEFT OUTER JOIN votes v ON v.post_id = p.post_id
+        JOIN subreddits s ON p.subreddit_id = s.subreddit_id
+        JOIN profiles pr ON pr.user_id = p.user_id
+        WHERE (
+          p.subreddit_id IN (SELECT subreddit_id FROM subreddit_membership WHERE user_id = $1)
+          OR p.subreddit_id IN (SELECT subreddit_id FROM subreddits WHERE admin_id = $1)
+        )
+        GROUP BY p.post_id, p.user_id, p.title, p.content, p.created_at, pr.username, s.subreddit_name, s.subreddit_id
+        ORDER BY p.created_at DESC`;
 
     const popularQuery = `WITH all_posts AS (
        SELECT p.post_id, p.user_id, p.title, p.content, p.created_at, s.subreddit_name, pr.username, SUM(v.vote_type) AS vote_score
