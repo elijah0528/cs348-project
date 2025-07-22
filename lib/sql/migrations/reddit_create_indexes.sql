788c3c73d7f4c3a4840d32ca1776382845947981
Rajan Agarwal <rajan.ag005@gmail.com>
2025-07-22 17:34:27 -0400

    implement collaborated new indexes for advanced feature 2!

--- lib/sql/migrations/reddit_create_indexes.sql
+++ lib/sql/migrations/reddit_create_indexes.sql
@@ -9,3 +9,7 @@ CREATE INDEX IF NOT EXISTS index_posts_subreddit_created_at_desc ON posts (subre
 CREATE INDEX IF NOT EXISTS index_votes_post_id_vote_type ON votes (post_id, vote_type);
 CREATE INDEX IF NOT EXISTS index_profiles_user_id ON profiles (user_id);
 CREATE INDEX IF NOT EXISTS index_posts_created_at_desc ON posts (created_at DESC);
+
+CREATE INDEX IF NOT EXISTS subreddit_membership_user_sub ON subreddit_membership (user_id, subreddit_id);
+CREATE INDEX IF NOT EXISTS index_votes_user_upvotes ON votes (user_id, post_id) WHERE vote_type = 1;
+CREATE INDEX IF NOT EXISTS index_votes_post_upvotes ON votes (post_id, user_id) WHERE vote_type = 1;
